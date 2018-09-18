aa46c31c47703529a856c229b40f6e163dcc714c
totason <jonkrona@gmail.com>
2018-09-18 10:51:32 +0000

    Tilraun 1, á eftir að runna, breytti í 5x5 grid og hoppa um tvo ef það er ekki veggur eftir 1

--- TileTraveller1.py
+++ TileTraveller1.py
@@ -6,9 +6,9 @@
 #Bæti við þeim upplýsingum að "Veggir" séu á ákveðnum stöðum
 #Skilgreini að ekki sé hægt að fara í gegnum þá með if elif 
 
-x = 1.0
-y = 1.0
-prufustrengur = str(x) + str(y)
+x = 1
+y = 1
+
 print(prufustrengur)
 norður = "(N)orth "
 austur = "(E)ast "
@@ -17,5 +17,36 @@ vestur = "(W)est "
 eða = "or "
 punktur = "."
 attir = ""
-badspots = ""
-#while (x != 3.0) and (y != 1.0):
+badspot1, badspot2, badspot3, badspot4 = "2.1", "3.4", "4.3", "4.1"
+
+while (x != 5) and (y != 1):
+    n = str(x) + "." + str(y + 1)
+    a = str(x + 1) + "." + str(y)
+    s = str(x) + "." + str(y - 1)
+    v = str(x - 1) + "." + str(y)
+    manordur, maaustur, masudur, mavestur = False
+
+    if (n != badspot1) and (n != badspot2) and (n != badspot3) and (n != badspot4) and ((y + 1)!= 6):
+        attir += norður
+        manordur = True
+    if (a != badspot1) and (a != badspot2) and (a != badspot3) and (a != badspot4) and ((x + 1)!= 6):
+        attir += austur
+        maaustur = True
+    if (s != badspot1) and (s != badspot2) and (s != badspot3) and (s != badspot4) and ((y - 1)!= 0):
+        attir += suður
+        masudur = True
+    if (v != badspot1) and (v != badspot2) and (v != badspot3) and (v != badspot4) and ((x - 1)!= 0):
+        attir += vestur
+        mavestur = True
+    print("You can travel: " + attir)
+    inp = input("Direction")
+
+    if (inp == "n" or inp =="N") and (manordur == True):
+        y += 2
+    elif (inp == "a" or inp =="A") and (maaustur == True):
+        x += 2
+    elif (inp == "s" or inp =="S") and (masudur == True):
+        y -= 2
+    elif (inp == "w" or inp =="W") and (mavestur == True):
+        x -= 2
+
