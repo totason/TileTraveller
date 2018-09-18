05de6008a6f5e750d788ef431f1a7195977aad37
totason <jonkrona@gmail.com>
2018-09-18 09:26:34 +0000

    Algorythm og byrjun

---
+++ TileTraveller1.py
@@ -0,0 +1,12 @@
+#x, y hnit fyrir staðsetningu
+#Ef notandi færir sig til norður eða suður þá er það + og - á y hniti
+#Austur og vestur eru + og - á x hniti
+#Ef x eða y er 1 kemst það ekki lengra í mínus áttina
+#Ef x eða y er 3 kemst það ekki lengra í + áttina
+#Bæti við þeim upplýsingum að "Veggir" séu á ákveðnum stöðum
+#Skilgreini að ekki sé hægt að fara í gegnum þá með if elif 
+
+x = 1
+y = 1
+while (x != 3) and (y != 1):
+    
