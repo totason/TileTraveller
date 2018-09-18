8d1cc1770ddfaf6e048eef6a4e31bdb554cc2704
totason <jonkrona@gmail.com>
2018-09-18 09:58:41 +0000

    Prófa að +a saman strengi, ekki að virka, lífið er tilgangslaust

--- TileTraveller1.py
+++ TileTraveller1.py
@@ -8,5 +8,18 @@
 
 x = 1
 y = 1
+norður = "(N)orth "
+austur = "(E)ast "
+suður = "(S)outh "
+vestur = "(W)est "
+eða = "or "
+punktur = "."
+attir = ""
+attir = norður + eða + austur + eða + suður + punktur
+attir -= austur
+print(attir)
 while (x != 3) and (y != 1):
+    if x == 1:
+        attir = norður + eða + austur + eða + suður, punktur
+
     
