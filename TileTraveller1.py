abb1f21da91dd87c200dd2967114e404cd0004aa
totason <jonkrona@gmail.com>
2018-09-18 11:28:38 +0000

    Þetta tókst með 5x5 aðferðinni, smá fínpúss eftir

--- TileTraveller1.py
+++ TileTraveller1.py
@@ -1,52 +1,75 @@
-#x, y hnit fyrir staðsetningu
-#Ef notandi færir sig til norður eða suður þá er það + og - á y hniti
+#x, y hnit fyrir stadsetningu
+#Ef notandi færir sig til nordur eda sudur þá er þad + og - á y hniti
 #Austur og vestur eru + og - á x hniti
-#Ef x eða y er 1 kemst það ekki lengra í mínus áttina
-#Ef x eða y er 3 kemst það ekki lengra í + áttina
-#Bæti við þeim upplýsingum að "Veggir" séu á ákveðnum stöðum
-#Skilgreini að ekki sé hægt að fara í gegnum þá með if elif 
+#Ef x eda y er 1 kemst þad ekki lengra í mínus áttina
+#Ef x eda y er 3 kemst þad ekki lengra í + áttina
+#Bæti vid þeim upplýsingum ad "Veggir" séu á ákvednum stödum
+#Skilgreini ad ekki sé hægt ad fara í gegnum þá med if elif 
 
 x = 1
 y = 1
-
-print(prufustrengur)
-norður = "(N)orth "
-austur = "(E)ast "
-suður = "(S)outh "
-vestur = "(W)est "
-eða = "or "
+nordur = "(N)orth"
+austur = " (E)ast"
+sudur = " (S)outh"
+vestur = " (W)est"
+eda = " or"
 punktur = "."
-attir = ""
-badspot1, badspot2, badspot3, badspot4 = "2.1", "3.4", "4.3", "4.1"
 
-while (x != 5) and (y != 1):
+badspot1, badspot2, badspot3, badspot4 = "2.1", "3.4", "4.3", "4.1"
+operator = (x != 5) and (y != 1)
+
+
+while True:
     n = str(x) + "." + str(y + 1)
     a = str(x + 1) + "." + str(y)
     s = str(x) + "." + str(y - 1)
     v = str(x - 1) + "." + str(y)
-    manordur, maaustur, masudur, mavestur = False
-
+    manordur, maaustur, masudur, mavestur = False, False, False, False
+    attir = ""
+    edaeftirfyrsta = False
     if (n != badspot1) and (n != badspot2) and (n != badspot3) and (n != badspot4) and ((y + 1)!= 6):
-        attir += norður
+        attir += nordur
         manordur = True
+        edaeftirfyrsta = True
     if (a != badspot1) and (a != badspot2) and (a != badspot3) and (a != badspot4) and ((x + 1)!= 6):
-        attir += austur
+        if edaeftirfyrsta:
+            attir = attir + eda + austur
+        else:
+            attir += austur
         maaustur = True
+        edaeftirfyrsta = True
     if (s != badspot1) and (s != badspot2) and (s != badspot3) and (s != badspot4) and ((y - 1)!= 0):
-        attir += suður
+        if edaeftirfyrsta:
+            attir = attir + eda + sudur
+        else:
+            attir += sudur
         masudur = True
+        edaeftirfyrsta = True
     if (v != badspot1) and (v != badspot2) and (v != badspot3) and (v != badspot4) and ((x - 1)!= 0):
-        attir += vestur
-        mavestur = True
-    print("You can travel: " + attir)
-    inp = input("Direction")
+        if edaeftirfyrsta:
+            attir = attir + eda + vestur
+        else:
+            attir += vestur
+        masudur = True
+    attir += punktur
 
-    if (inp == "n" or inp =="N") and (manordur == True):
-        y += 2
-    elif (inp == "a" or inp =="A") and (maaustur == True):
-        x += 2
-    elif (inp == "s" or inp =="S") and (masudur == True):
-        y -= 2
-    elif (inp == "w" or inp =="W") and (mavestur == True):
-        x -= 2
+    virkadi = False
+    while virkadi == False:
+        print("You can travel: " + attir)
+        inp = input("Direction: ")
+        virkadi = True
+        if (inp == "n" or inp =="N") and (manordur == True):
+            y += 2
+        elif (inp == "e" or inp =="E") and (maaustur == True):
+            x += 2
+        elif (inp == "s" or inp =="S") and (masudur == True):
+            y -= 2
+        elif (inp == "w" or inp =="W") and (mavestur == True):
+            x -= 2
+        else:
+            print("Invalid direction!")
+            virkadi = False
+    if (x == 5) and (y == 1):
+        print("Victory Royale")
+        break
 
